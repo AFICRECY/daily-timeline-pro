5249ef7369106f97f44526e75fb48415252cd38e
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-04-06 23:26:37 -0700

    added the final function and fixed the layout, final touches

--- assets/script.js
+++ assets/script.js
@@ -45,25 +45,23 @@ $(function () {
 //getting the data from localStorage whatever we have (can be null)
 //with the loop we are trying the get the data for each row using id.
 
+initState();
+//calling the function.
 
-
-
-//   <!--5. When save is clicked, text is saved to localstorage  -->
-// create a queryselectorall for the save buttons
-// create event listner for save button
-// capture the text in a variable
-// save text to localstorage 
-// innerHTML means it will save to the screen 
-// local 
 
 
 // stored everything into the full date variable 
 // We are trying to get the month alone
 // we are setting all the values(day, month, year) to one variable 
-var fullDate = new Date()
-var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
-var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-document.getElementById("datepicker").innerHTML=currentDate
+var fullDate = new Date();
+var twoDigitMonth =
+ fullDate.getMonth().length + 1 === 1
+   ? fullDate.getMonth() + 1
+   : "0" + (fullDate.getMonth() + 1);
+var currentDate =
+ fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+document.getElementById("datepicker").innerHTML = currentDate;
+
 // one line 46 we stored the data into the data variable 
 // we are printing it on the indexhtml file 
 
@@ -106,17 +104,52 @@ Array.from(rows).forEach(row => {
    }
  }
 
-  
-  
-  
-  // var data = localStorage.getItem(row.id)
-  // if(localStorage.hasOwnProperty(row.id)){
-  //   console.log("data" + data)
-  // }
-  
+ var savedBtn = Array.from(document.querySelectorAll(".saveBtn"));
+ savedBtn.forEach((btn) => {
+   btn.addEventListener("click", handleSave);
+ });
+//Using forEach loop we are traversing the rows, we match the time with row time, and according the condition we change the color
+//handlesave is used to save the planner that we have type in the row
 
 
+
+// This is a JavaScript code snippet using the jQuery library.
+// The first line declares a variable named btnId and assigns it the value of the id property of the parent element of the current 
+// element, accessed using jQuery's parent() method.
+function handleSave(e) {
+   var btnId = $(this).parent()[0].id;
+   var eventText = $(this).siblings("textarea").val();
+   //storing the text that we write
+   setData(btnId, eventText);
+   console.log("Button : " + eventText, btnId);
+ }
+
+
+ function setData(label, value) {
+   var data = localStorage.getItem("data");
+   var resObj = {};
+   if (data) {
+     data = JSON.parse(data);
+     data = { ...data, ...{ [label]: value } };
+     resObj = data;
+   } else {
+     resObj[label] = value;
+   }
+   localStorage.setItem("data", JSON.stringify(resObj));
+ }
 });
+//this function mainly helps to set data and to append the new data into the old one.
+// This code snippet checks if the variable data exists, and if it does, it is parsing the JSON data using JSON.parse method.
+// Then, it is creating a new object by spreading the data object and adding a new property with a key label and value value
+
+
 
 //--------------------------------------
 
+//    <!--5. When save is clicked, text is saved to localstorage  -->
+// create a queryselectorall for the save buttons
+// create event listner for save button
+// capture the text in a variable
+// save text to localstorage 
+// innerHTML means it will save to the screen 
+// local 
